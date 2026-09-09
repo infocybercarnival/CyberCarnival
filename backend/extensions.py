aa1f51f8043c27f36378c6478c124a5fbfa36660
infocybercarnival <info.cybercarnival@gmail.com>
2026-09-09 18:17:18 +0530

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,4 +1,3 @@
-from flask import request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_wtf import CSRFProtect
@@ -7,13 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 import config
 
 
-def get_client_ip():
-    forwarded = request.headers.get("X-Forwarded-For")
-    if forwarded:
-        return forwarded.split(",")[0].strip()
-    return get_remote_address()
+# request.remote_addr is corrected by ProxyFix in app.py.
+# Do not parse X-Forwarded-For manually here.
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri=config.RATELIMIT_STORAGE_URI,
+)
 
-
-limiter = Limiter(key_func=get_client_ip, storage_uri=config.RATELIMIT_STORAGE_URI)
 csrf = CSRFProtect()
 db = SQLAlchemy()
